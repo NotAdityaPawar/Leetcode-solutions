f42efd479a75988ccc30aba658f2e960ed895903
Aditya Pawar <52417220+NotAdityaPawar@users.noreply.github.com>
2022-07-29 17:20:26 +0530

    Time: 56 ms (67.06%), Space: 36.8 MB (62.68%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -13,18 +13,17 @@ class Solution {
 public:
     void inorder(TreeNode* root,vector<int> &ans){
         if (root==NULL) return;
-        if (root->right) inorder(root->right,ans);
-        ans.push_back(root->val);
         if (root->left) inorder(root->left,ans);
+        ans.push_back(root->val);
+        if (root->right) inorder(root->right,ans);
     }
     bool findTarget(TreeNode* root, int k) {
         vector<int> ans;
         inorder(root,ans);
+        
         for (int i=0;i<ans.size();i++){
             for (int j = i+1;j<ans.size();j++){
-                if (ans[i]+ans[j]==k){
-                    return true;
-                }
+                if (ans[i] + ans[j]==k) return true;
             }
         }
         return false;
