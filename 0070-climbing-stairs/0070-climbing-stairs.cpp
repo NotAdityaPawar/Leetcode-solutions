fd7820ea34b4ea08b1f5a8b9846f585fe105f9d0
Aditya Pawar <52417220+NotAdityaPawar@users.noreply.github.com>
2022-12-12 07:18:19 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (38.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> fib(n+2,0);
+        
+        fib[0] = 0;
+        fib[1] = 1;
+        
+        for (int i = 2;i<=n+1;i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[n+1];
+    }
+};
