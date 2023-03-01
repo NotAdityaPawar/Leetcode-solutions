32e2614bab0b6cc57c78f3ef871fe5831935c4e2
Aditya Pawar <52417220+NotAdityaPawar@users.noreply.github.com>
2023-03-01 16:53:48 +0530

    Time: 832 ms (86.03%), Space: 27.2 MB (13.78%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,51 +1,25 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        temp_arr = [0]*len(nums)
         
-        def merge(left,mid,right):
-            start1 = left
-            start2 = mid + 1
-            n1 = mid-left + 1
-            n2 = right-mid
-            
-            for i in range(n1):
-                temp_arr[start1+i] = nums[start1+i]
-            for i in range(n2):
-                temp_arr[start2 + i] = nums[start2 + i]
-                
-            i = 0
-            j = 0
-            k = left
-            
-            while i<n1 and j<n2:
-                if temp_arr[start1 + i] <=temp_arr[start2+j]:
-                    nums[k] = temp_arr[start1+i]
-                    i += 1
+        def counting_sort():
+            counts = {}
+            minVal = min(nums)
+            maxVal = max(nums)
+
+            for num in nums:
+                counts[num] = counts.get(num,0) + 1
+
+            index = 0
+
+            for val in range(minVal,maxVal+1,1):
+                while counts.get(val,0)>0:
+                    nums[index] = val
+                    index += 1
+
+                    counts[val] -= 1
                     
-                else:
-                    nums[k] = temp_arr[start2 + j]
-                    j += 1
-                k += 1
-                
-            while i<n1:
-                nums[k] = temp_arr[start1 + i]
-                i += 1
-                k += 1
             
-            while j<n2:
-                nums[k] = temp_arr[start2 + j]
-                j += 1
-                k += 1
-        
-        
-        def merge_sort(left,right):
-            if left>=right:
-                return
-            mid = (left+right)//2
-            merge_sort(left,mid)
-            merge_sort(mid+1,right)
-            merge(left,mid,right)
-        merge_sort(0,len(nums)-1)
-        
+        counting_sort()
         return nums
-            
+                
+        
