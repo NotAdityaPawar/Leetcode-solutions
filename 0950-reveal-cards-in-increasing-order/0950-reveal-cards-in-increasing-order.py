1edc646ccc7b0fc1bcaf88089b06072ddf002cc0
Aditya Pawar <52417220+NotAdityaPawar@users.noreply.github.com>
2024-04-11 05:13:13 +0530

    Time: 42 ms (80.18%), Space: 16.8 MB (80.89%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -0,0 +1,20 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        n = len(deck)
+        
+        queue = deque()
+        for i in range(n):
+            queue.append(i)
+            
+        deck.sort()
+        
+        ans = [0]*n
+        
+        for card in deck:
+            ans[queue.popleft()] = card
+            
+            if queue:
+                queue.append(queue.popleft())
+                
+                
+        return ans
